daaf35667bba79d8a65a594952b16e800a5a2003
Ubuntu <ubuntu@ip-172-31-33-27.eu-central-1.compute.internal>
2019-12-02 11:16:27 +0000

    commit dec2

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow-ssh" {
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = "vpc-0a657c4ad2e27235d"
   name        = "allow-ssh"
   description = "security group that allows ssh and all egress traffic"
   egress {
--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,19 @@
 # Internet VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  tags = {
-    Name = "main"
-  }
-}
+#resource "aws_vpc" "main" {
+#  cidr_block           = "10.0.0.0/16"
+#  instance_tenancy     = "default"
+#  enable_dns_support   = "true"
+#  enable_dns_hostnames = "true"
+#  enable_classiclink   = "false"
+#  tags = {
+#    Name = "main"
+#  }
+#}
 
 # Subnets
 resource "aws_subnet" "main-public" {
-  vpc_id                  = aws_vpc.main.id
+#  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-0a657c4ad2e27235d"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-central-1b"
@@ -23,7 +24,8 @@ resource "aws_subnet" "main-public" {
 }
 
 resource "aws_subnet" "main-private" {
-  vpc_id                  = aws_vpc.main.id
+#  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "vpc-0a657c4ad2e27235d"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-central-1b"
@@ -35,20 +37,20 @@ resource "aws_subnet" "main-private" {
 
 
 # Internet GW
-resource "aws_internet_gateway" "main-gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
+#resource "aws_internet_gateway" "main-gw" {
+#  vpc_id = aws_vpc.main.id
+#
+#  tags = {
+#    Name = "main"
+#  }
+#}
 
 # route tables
 resource "aws_route_table" "main-public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "vpc-0a657c4ad2e27235d"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-gw.id
+    gateway_id = "igw-03a9174fb1381c408"
   }
 
   tags = {
@@ -73,12 +75,12 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.main-public.id
-  depends_on    = [aws_internet_gateway.main-gw]
+#  depends_on    = "igw-03a9174fb1381c408"
 }
 
 # VPC setup for NAT
 resource "aws_route_table" "main-private" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "vpc-0a657c4ad2e27235d"
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gw.id
