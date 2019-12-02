5d0fefa4fae55e2803742a57a71f1139e0157ab5
Ubuntu <ubuntu@ip-172-31-33-27.eu-central-1.compute.internal>
2019-12-02 12:20:47 +0000

    commit dec2

--- securitygroup.tf
+++ securitygroup.tf
@@ -20,3 +20,25 @@ resource "aws_security_group" "allow-ssh" {
   }
 }
 
+resource "aws_security_group" "allow-http" {
+  vpc_id      = "vpc-0a657c4ad2e27235d"
+  name        = "allow-http"
+  description = "security group that allows http and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-http"
+  }
+}
+
