5ce5f2a126eb9776d8ac9efdd37e85a36f0d4d3e
Ubuntu <ubuntu@ip-10-0-100-9.eu-central-1.compute.internal>
2019-12-04 08:44:33 +0000

    commit dec 4

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,5 @@
-# Internet VPC
-#resource "aws_vpc" "main" {
-#  cidr_block           = "10.0.0.0/16"
-#  instance_tenancy     = "default"
-#  enable_dns_support   = "true"
-#  enable_dns_hostnames = "true"
-#  enable_classiclink   = "false"
-#  tags = {
-#    Name = "main"
-#  }
-#}
-
 # Subnets
 resource "aws_subnet" "main-public" {
-#  vpc_id                  = aws_vpc.main.id
   vpc_id                  = "vpc-0a657c4ad2e27235d"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
@@ -24,7 +11,6 @@ resource "aws_subnet" "main-public" {
 }
 
 resource "aws_subnet" "main-private" {
-#  vpc_id                  = aws_vpc.main.id
   vpc_id                  = "vpc-0a657c4ad2e27235d"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
@@ -37,13 +23,6 @@ resource "aws_subnet" "main-private" {
 
 
 # Internet GW
-#resource "aws_internet_gateway" "main-gw" {
-#  vpc_id = aws_vpc.main.id
-#
-#  tags = {
-#    Name = "main"
-#  }
-#}
 
 # route tables
 resource "aws_route_table" "main-public" {
@@ -75,7 +54,6 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.main-public.id
-#  depends_on    = "igw-03a9174fb1381c408"
 }
 
 # VPC setup for NAT
